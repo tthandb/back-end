e779526652ed8b22a3495882f1a149174737139b
tthandb <tthandb@hotmail.com>
2020-11-24 22:43:16 +0700

    lklk

--- todo.sql
+++ todo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 23, 2020 at 04:34 PM
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2020 at 04:42 PM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,24 +44,6 @@ INSERT INTO `api_auth` (`id`, `api_token`, `date_added`, `date_modified`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `product_name` varchar(100) NOT NULL,
-  `product_desc` varchar(100) NOT NULL,
-  `product_price` int(11) NOT NULL,
-  `product_sku` varchar(100) NOT NULL,
-  `product_url` varchar(100) NOT NULL,
-  `date_added` datetime DEFAULT NULL,
-  `date_modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `projects`
 --
 
@@ -69,6 +51,37 @@ CREATE TABLE `projects` (
   `project_id` int(11) NOT NULL,
   `project_name` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`project_id`, `project_name`) VALUES
+(1, 'project 1'),
+(4, 'project 4'),
+(5, 'project 5'),
+(6, 'project 6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE `statuses` (
+  `id` tinyint(1) NOT NULL,
+  `name` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `name`) VALUES
+(0, 'Unassigned'),
+(1, 'To Do'),
+(2, 'Doing'),
+(3, 'Done');
 
 -- --------------------------------------------------------
 
@@ -78,10 +91,23 @@ CREATE TABLE `projects` (
 
 CREATE TABLE `tasks` (
   `task_id` int(11) NOT NULL,
-  `task_title` int(11) NOT NULL,
+  `task_title` varchar(255) NOT NULL,
+  `project_id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
-  `create_at` datetime NOT NULL
+  `status_id` tinyint(1) NOT NULL DEFAULT 0,
+  `create_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`task_id`, `task_title`, `project_id`, `user_id`, `status_id`, `create_at`) VALUES
+(4, 'task 4', 1, 4, 0, '2020-11-24 01:40:05'),
+(5, 'task 4', 6, 4, 2, '2020-11-24 15:47:23'),
+(6, 'task 5', 4, 4, 2, '2020-11-24 15:48:41'),
+(12, 'task 6', 5, 5, 0, '2020-11-24 20:44:58'),
+(13, 'task 7', 1, 5, 0, '2020-11-24 20:46:24');
 
 -- --------------------------------------------------------
 
@@ -91,20 +117,18 @@ CREATE TABLE `tasks` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `user_role` varchar(50) NOT NULL,
-  `auth_token` varchar(100) NOT NULL,
-  `date_added` datetime DEFAULT NULL,
-  `date_modified` datetime DEFAULT NULL
+  `auth_token` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `user_role`, `auth_token`, `date_added`, `date_modified`) VALUES
-(2, 'abc@xyz.com', '123', 'user', 'as8RS$KaJ37GO02@rejQyc9MA@qfBlcV', NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `password`, `auth_token`) VALUES
+(4, 'binhnd', '123', 'TLy&D#aENLPm889Yh22eGlI#bImr7t1u'),
+(5, 'test', '1234', 'k@oPotFQ@12@u9jFtssOf4K&xLTIyFUZ');
 
 --
 -- Indexes for dumped tables
@@ -117,30 +141,32 @@ ALTER TABLE `api_auth`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
 -- Indexes for table `projects`
 --
 ALTER TABLE `projects`
   ADD PRIMARY KEY (`project_id`);
 
 --
+-- Indexes for table `statuses`
+--
+ALTER TABLE `statuses`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`task_id`);
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `project_id` (`project_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `status_id` (`status_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email_UNIQUE` (`email`);
+  ADD UNIQUE KEY `email_UNIQUE` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -153,38 +179,34 @@ ALTER TABLE `api_auth`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `products`
+-- Constraints for table `tasks`
 --
-ALTER TABLE `products`
-  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`),
+  ADD CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `tasks_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
